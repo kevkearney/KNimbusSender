5c9c68fec2f6c58e6ce1aa7acb886fa7b3e9e948
Kevin <kevkearney@gmail.com>
2017-05-03 18:02:49 -0400

    Changed folder structure

---
+++ KnimbusRadio.h
@@ -0,0 +1,32 @@
+/*
+  KnimbusRadio.h - Library for using RF24 radio with knimbus weather station.
+  Created by Kevin D. Kearney, April 30, 2017.
+  Released into the public domain.
+*/
+
+#ifndef KnimbusRadio_h
+#define KnimbusRadio_h
+
+#include <RF24.h>
+#include <Arduino.h>
+
+//Weather Struct
+typedef struct {
+  float Temperature;
+  unsigned int Humidity;
+  float BaroPressure;
+  float BaroTemperature;
+  float Lux;
+}
+Weather_t;
+
+class KnimbusRadio
+{
+  public:
+    void SetupRadio();
+    bool XMitWeather(Weather_t weatherData, const String &responseMsg);
+    bool XMitLightning(String lightningType, int lightningDistance);
+  private:
+    bool PowerOnRadioAndXMit(void* buf, int size, const String &responseMsg);
+};
+#endif
